75a3d8d6fc51773330e045f7663d91f98ffc8ccf
Ringo Singson <ringosingson@live.com>
2020-02-05 00:44:12 +0530

    setting up graphql schema

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,13 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
+
 const app = express();
 const port = 9000;
 
-const one = 1;
-const two = 2;
+const server = new ApolloServer({ schema });
 
-app.use(bodyParser.json());
-
-app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`));
-
-app.get('/listings', (_req, res) => {
-  res.send(listings);
-});
-
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1)[0]);
-    }
-  }
-
-  return res.send('failed to deleted listing');
-});
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port);
 
